43bf9178e4a3d48f5ead39533e36f987d71aac8a
sayyad-mukthumsa-123 <sayyad.mukthumsa@gmail.com>
2024-07-13 22:45:08 +0530

    version3

--- client/src/register.js
+++ client/src/register.js
@@ -29,7 +29,7 @@ const Register = () => {
         }
 
         try {
-            const response = await axios.post('http://localhost:5000/register', formData, {
+            const response = await axios.post('https://dev-mania-xi.vercel.app//register', formData, {
                 headers: {
                     'Content-Type': 'application/json',
                 }
